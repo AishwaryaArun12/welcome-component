0fdee83ba0f39b1b407ccdd10b317852d7d2a842
AishwaryaArun12 <jj4245888@gmail.com>
2022-07-06 11:03:05 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { Welcome } from './component/Welcome';
+
+function App() {
+
+  return (
+    <div className="App">
+     <Welcome name="sara"></Welcome>
+    </div>
+  );
+}
+
+export default App;
